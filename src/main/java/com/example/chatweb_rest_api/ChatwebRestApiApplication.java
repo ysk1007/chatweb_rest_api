e9b19cbd3bc9598681ff2c7393ab4dbb9243cd9b
okonys <62321546+ysk1007@users.noreply.github.com>
2025-07-17 12:07:38 +0900

    빈 프로젝트 생성

---
+++ src/main/java/com/example/chatweb_rest_api/ChatwebRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatweb_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatwebRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatwebRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/chatweb_rest_api/ChatwebRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatweb_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatwebRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
